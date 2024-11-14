88d9ed8c2a9bfd4e2f12c635241346a2d9c94d25
astral4 <88992929+astral4@users.noreply.github.com>
2024-11-14 13:14:03 +0000

    refactor; read article text

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use anyhow::{anyhow, Context, Result};
-use std::{fs::read_dir, path::PathBuf};
+use std::{
+    fs::{read_dir, read_to_string},
+    path::PathBuf,
+};
+use tap::Pipe;
 
 fn main() -> Result<()> {
     let content_path: PathBuf = std::env::args()
@@ -14,12 +18,10 @@ fn main() -> Result<()> {
             .context("failed to access article directory")?
             .path();
 
-        let article_path = read_dir(article_dir_path)
-            .context("failed to start traversal of article directory")?
-            .filter_map(Result::ok)
-            .map(|entry| entry.path())
-            .find(|path| path.is_file() && path.file_name().unwrap() == "index.md")
-            .ok_or_else(|| anyhow!("failed to find article text file"))?;
+        let article_text = article_dir_path
+            .join("index.md")
+            .pipe(read_to_string)
+            .context("failed to read article text file")?;
     }
 
     Ok(())
