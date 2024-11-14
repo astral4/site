8a80b4c6f8bf0694af42275fd0097e5139370906
astral4 <88992929+astral4@users.noreply.github.com>
2024-11-14 03:20:35 +0000

    get article path

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,22 @@ use std::{fs::read_dir, path::PathBuf};
 fn main() -> Result<()> {
     let content_path: PathBuf = std::env::args()
         .next()
-        .ok_or_else(|| anyhow!("path to content was not provided"))?
+        .ok_or_else(|| anyhow!("path to articles was not provided"))?
         .into();
 
     for article_dir in
-        read_dir(content_path).context("failed to start traversal of content at supplied path")?
+        read_dir(content_path).context("failed to start traversal of all articles")?
     {
         let article_dir_path = article_dir
-            .context("failed to access content entry")?
+            .context("failed to access article directory")?
             .path();
+
+        let article_path = read_dir(article_dir_path)
+            .context("failed to start traversal of article directory")?
+            .filter_map(Result::ok)
+            .map(|entry| entry.path())
+            .find(|path| path.is_file() && path.file_name().unwrap() == "index.md")
+            .ok_or_else(|| anyhow!("failed to find article text file"))?;
     }
 
     Ok(())
