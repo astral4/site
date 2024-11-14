9f18c49206f61ad5c86f58daa2da210f9df84782
astral4 <88992929+astral4@users.noreply.github.com>
2024-11-14 02:31:54 +0000

    traverse content directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    todo!()
+use anyhow::{anyhow, Context, Result};
+use walkdir::WalkDir;
+
+fn main() -> Result<()> {
+    let content_path = std::env::args()
+        .next()
+        .ok_or_else(|| anyhow!("file path to content was not provided"))?;
+
+    for entry in WalkDir::new(content_path) {
+        let file = entry.context("failed to read content entry")?;
+    }
+
+    Ok(())
 }
