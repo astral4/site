a0958d1e87cc56b1e8a7e6092c34a087acbb71f4
astral4 <88992929+astral4@users.noreply.github.com>
2024-11-15 08:14:14 +0000

    implement LaTeX to HTML conversion

---
+++ katex/README.md
@@ -0,0 +1,2 @@
+Current version: `katex 0.16.11`
+Source: https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.js
---
+++ src/lib.rs
@@ -0,0 +1,96 @@
+use anyhow::{Context as _, Result};
+use rquickjs::{Context, Function, Object, Runtime};
+
+const KATEX_SRC: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/katex/katex.min.js"));
+
+pub struct KatexEngine {
+    context: Context,
+}
+
+#[derive(Clone, Copy)]
+pub enum RenderMode {
+    Inline,
+    Display,
+}
+
+impl KatexEngine {
+    /// # Errors
+    /// This function returns an error for:
+    /// - failed initialization of the underlying JavaScript runtime from `rquickjs`
+    /// - failed evaluation of the embedded `katex` source code
+    pub fn new() -> Result<Self> {
+        let runtime = Runtime::new().context("failed to initialize JS runtime")?;
+        let context = Context::full(&runtime).context("failed to initialize JS runtime context")?;
+
+        context
+            .with(|ctx| {
+                ctx.eval::<(), _>(KATEX_SRC)
+                    .context("failed to evaluate `katex` source code")
+            })
+            .context("failed to initialize `katex`")?;
+
+        Ok(Self { context })
+    }
+
+    /// # Errors
+    /// This function returns an error if
+    /// - the rendering settings could not be initialized
+    /// - the `katex.renderToString()` function could not be found
+    /// - the `katex.renderToString()` function failed to run
+    pub fn latex_to_html(&self, src: &str, mode: RenderMode) -> Result<String> {
+        self.context.with(|ctx| {
+            let settings =
+                Object::new(ctx.clone()).context("failed to initialize `katex` settings")?;
+            settings
+                .set(
+                    "displayMode",
+                    match mode {
+                        RenderMode::Inline => false,
+                        RenderMode::Display => true,
+                    },
+                )
+                .context("failed to initialize `katex` settings")?;
+
+            ctx.globals()
+                .get::<_, Object<'_>>("katex")
+                .context("failed to find `katex` namespace")?
+                .get::<_, Function<'_>>("renderToString")
+                .context("failed to find `katex.renderToString()`")?
+                .call((src, settings))
+                .context("failed to run `katex.renderToString()`")
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{KatexEngine, RenderMode};
+
+    #[test]
+    fn latex_to_html() {
+        let engine = KatexEngine::new().expect("engine initialization should succeed");
+
+        let inline_html = engine
+            .latex_to_html("2x+3y=4z", RenderMode::Inline)
+            .expect("inline LaTeX conversion should succeed");
+
+        let display_html = engine
+            .latex_to_html("2x+3y=4z", RenderMode::Display)
+            .expect("display LaTeX conversion should succeed");
+
+        assert_ne!(
+            inline_html, display_html,
+            "inline LaTeX and display LaTeX should yield different outputs"
+        );
+    }
+
+    #[should_panic = "conversion should fail on invalid LaTeX"]
+    #[test]
+    fn invalid_latex() {
+        let engine = KatexEngine::new().expect("engine initialization should succeed");
+
+        engine
+            .latex_to_html("\\frac{", RenderMode::Inline)
+            .expect("conversion should fail on invalid LaTeX");
+    }
+}
