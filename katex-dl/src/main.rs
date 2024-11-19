326dfb2fd7f604fea3416c9620f651f516882832
astral4 <88992929+astral4@users.noreply.github.com>
2024-11-19 11:47:52 +0000

    construct HTTP client

--- katex-dl/src/main.rs
+++ katex-dl/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
+use anyhow::{Context, Result};
+use reqwest::Client;
+use std::time::Duration;
+
+const BASE_URL: &str = "https://cdn.jsdelivr.net/npm/katex/dist/";
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let client = Client::builder()
+        .https_only(true)
+        .timeout(Duration::from_secs(15))
+        .use_rustls_tls()
+        .build()
+        .context("failed to build HTTP client")?;
+
     todo!()
 }
