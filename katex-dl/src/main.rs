ac301a1a3491c19ac80508dd696efb97f6de6be3
astral4 <88992929+astral4@users.noreply.github.com>
2024-11-19 11:56:52 +0000

    fetch JS source

--- katex-dl/src/main.rs
+++ katex-dl/src/main.rs
@@ -2,7 +2,7 @@ use anyhow::{Context, Result};
 use reqwest::Client;
 use std::time::Duration;
 
-const BASE_URL: &str = "https://cdn.jsdelivr.net/npm/katex/dist/";
+const JS_URL: &str = "https://cdn.jsdelivr.net/npm/katex/dist/katex.min.js";
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -13,5 +13,14 @@ async fn main() -> Result<()> {
         .build()
         .context("failed to build HTTP client")?;
 
-    todo!()
+    let js_source = client
+        .get(JS_URL)
+        .send()
+        .await
+        .context("failed to fetch KaTeX JS source")?
+        .text()
+        .await
+        .context("failed to convert JS fetch response to text")?;
+
+    Ok(())
 }
