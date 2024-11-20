a684e0b385399362e22455f9ef290e9230dac270
astral4 <88992929+astral4@users.noreply.github.com>
2024-11-20 00:39:14 +0000

    get version of latest KaTeX distribution

--- katex-dl/src/main.rs
+++ katex-dl/src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::{Context, Result};
+use regex::Regex;
 use reqwest::Client;
 use std::time::Duration;
 
@@ -13,6 +14,8 @@ async fn main() -> Result<()> {
         .build()
         .context("failed to build HTTP client")?;
 
+    let version_matcher = Regex::new(r#"version:"([0-9]+?\.[0-9]+?\.[0-9]+?)""#).unwrap();
+
     let js_source = client
         .get(JS_URL)
         .send()
@@ -21,6 +24,12 @@ async fn main() -> Result<()> {
         .text()
         .await
         .context("failed to convert JS fetch response to text")?;
+
+    let version = version_matcher
+        .captures(&js_source)
+        .unwrap()
+        .extract::<1>()
+        .1[0];
 
     Ok(())
 }
