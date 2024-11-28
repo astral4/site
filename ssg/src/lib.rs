773166d244b50aac04360a75c4136e05de859aa3
astral4 <88992929+astral4@users.noreply.github.com>
2024-11-28 15:50:17 +0000

    refactor Markdown parser builder

--- ssg/src/lib.rs
+++ ssg/src/lib.rs
@@ -14,21 +14,5 @@ pub use highlight::SyntaxHighlighter;
 pub use image::process_image;
 pub use latex::{LatexConverter, RenderMode};
 
-use pulldown_cmark::{Event, Options, Parser, TextMergeStream};
-
 pub const OUTPUT_CSS_DIRECTORY: &str = "/stylesheets/";
 pub const OUTPUT_SITE_CSS_FILE: &str = "/stylesheets/site.css";
-
-/// Parses the input string as Markdown, returning an iterator of Markdown parsing events.
-/// The parser recognizes the following extensions to the CommonMark standard:
-/// - strikethroughs
-/// - YAML-style frontmatter
-/// - math markup
-pub fn parse_markdown(text: &str) -> impl Iterator<Item = Event<'_>> {
-    TextMergeStream::new(Parser::new_ext(
-        text,
-        Options::ENABLE_STRIKETHROUGH
-            | Options::ENABLE_YAML_STYLE_METADATA_BLOCKS
-            | Options::ENABLE_MATH,
-    ))
-}
--- ssg/src/main.rs
+++ ssg/src/main.rs
@@ -1,9 +1,11 @@
 use anyhow::{anyhow, Context, Result};
 use foldhash::{HashSet, HashSetExt};
-use pulldown_cmark::{html::push_html, CodeBlockKind, Event, Tag, TagEnd};
+use pulldown_cmark::{
+    html::push_html, CodeBlockKind, Event, Options, Parser, Tag, TagEnd, TextMergeStream,
+};
 use ssg::{
-    parse_markdown, process_image, transform_css, Config, CssOutput, Frontmatter, LatexConverter,
-    PageBuilder, RenderMode, SyntaxHighlighter, OUTPUT_CSS_DIRECTORY, OUTPUT_SITE_CSS_FILE,
+    process_image, transform_css, Config, CssOutput, Frontmatter, LatexConverter, PageBuilder,
+    RenderMode, SyntaxHighlighter, OUTPUT_CSS_DIRECTORY, OUTPUT_SITE_CSS_FILE,
 };
 use std::fs::{create_dir, create_dir_all, read_dir, read_to_string, write};
 
@@ -120,55 +122,60 @@ fn main() -> Result<()> {
             let mut contains_math = false;
 
             // Convert article from Markdown to HTML
-            let events = parse_markdown(&article_text)
-                .map(|event| match event {
-                    Event::Start(Tag::CodeBlock(ref kind)) => {
-                        is_in_code_block = true;
-                        code_language = match kind {
-                            CodeBlockKind::Indented => None,
-                            CodeBlockKind::Fenced(lang) => (!lang.is_empty()).then(|| lang.clone()),
-                        };
-                        Ok(event)
-                    }
-                    Event::End(TagEnd::CodeBlock) => {
-                        is_in_code_block = false;
-                        Ok(event)
-                    }
-                    Event::Text(text) if is_in_code_block => syntax_highlighter
-                        .highlight(&text, code_language.as_deref())
-                        .context("failed to highlight text block")
-                        .map(|html| Event::InlineHtml(html.into())),
-                    Event::Start(Tag::Image {
-                        dest_url,
-                        title,
-                        id,
-                        ..
-                    }) => process_image(
-                        &input_article_dir,
-                        &output_article_dir,
-                        &dest_url,
-                        &title,
-                        &id,
-                    )
-                    .context("failed to process image")
+            let events = TextMergeStream::new(Parser::new_ext(
+                &article_text,
+                Options::ENABLE_STRIKETHROUGH
+                    | Options::ENABLE_YAML_STYLE_METADATA_BLOCKS
+                    | Options::ENABLE_MATH,
+            ))
+            .map(|event| match event {
+                Event::Start(Tag::CodeBlock(ref kind)) => {
+                    is_in_code_block = true;
+                    code_language = match kind {
+                        CodeBlockKind::Indented => None,
+                        CodeBlockKind::Fenced(lang) => (!lang.is_empty()).then(|| lang.clone()),
+                    };
+                    Ok(event)
+                }
+                Event::End(TagEnd::CodeBlock) => {
+                    is_in_code_block = false;
+                    Ok(event)
+                }
+                Event::Text(text) if is_in_code_block => syntax_highlighter
+                    .highlight(&text, code_language.as_deref())
+                    .context("failed to highlight text block")
                     .map(|html| Event::InlineHtml(html.into())),
-                    Event::InlineMath(src) => {
-                        contains_math = true;
-                        latex_converter
-                            .latex_to_html(&src, RenderMode::Inline)
-                            .context("failed to convert LaTeX to HTML")
-                            .map(|html| Event::InlineHtml(html.into()))
-                    }
-                    Event::DisplayMath(src) => {
-                        contains_math = true;
-                        latex_converter
-                            .latex_to_html(&src, RenderMode::Display)
-                            .context("failed to convert LaTeX to HTML")
-                            .map(|html| Event::InlineHtml(html.into()))
-                    }
-                    _ => Ok(event),
-                })
-                .collect::<Result<Vec<_>>>()?;
+                Event::Start(Tag::Image {
+                    dest_url,
+                    title,
+                    id,
+                    ..
+                }) => process_image(
+                    &input_article_dir,
+                    &output_article_dir,
+                    &dest_url,
+                    &title,
+                    &id,
+                )
+                .context("failed to process image")
+                .map(|html| Event::InlineHtml(html.into())),
+                Event::InlineMath(src) => {
+                    contains_math = true;
+                    latex_converter
+                        .latex_to_html(&src, RenderMode::Inline)
+                        .context("failed to convert LaTeX to HTML")
+                        .map(|html| Event::InlineHtml(html.into()))
+                }
+                Event::DisplayMath(src) => {
+                    contains_math = true;
+                    latex_converter
+                        .latex_to_html(&src, RenderMode::Display)
+                        .context("failed to convert LaTeX to HTML")
+                        .map(|html| Event::InlineHtml(html.into()))
+                }
+                _ => Ok(event),
+            })
+            .collect::<Result<Vec<_>>>()?;
 
             let mut article_body = String::with_capacity(article_text.len() * 3 / 2);
             push_html(&mut article_body, events.into_iter());
